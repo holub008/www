96c21f0c824c1e96a78bd3440196b7f14ce699f9
Karl Holub <holub008@umn.edu>
2019-05-25 03:17:01 -0500

    filling in some content

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use('/', express.static('public'));
-app.use('/analysis/', express.static('public/analyses'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/not_found.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
