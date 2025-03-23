9fc9ff94b290fcaf8c9119e27fcbd0b5084d57d3
Karl Holub <karljholub@gmail.com>
2025-03-23 01:41:32 -0500

    couple important dates

--- server/index.js
+++ server/index.js
@@ -4,7 +4,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static('public'));
+app.use('/', express.static('public', {
+  setHeaders: (res, filePath) => {
+    if (!filePath.endsWith('.html')) {
+      const ext = path.extname(filePath);
+      if (!ext) {
+        res.setHeader('Content-Type', 'text/html');
+      }
+    }
+  },
+}));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/not_found.html'));
